8a089534e391e589bb1cde9b730f69b54094814d
ainadlina <ainadlina14@gmail.com>
2025-01-12 14:31:15 +0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Popular from "./components/Popular";
+import Adventure from "./components/Adventure";
+import Footer from "./components/Footer";
+import Foodie from "./components/Foodie";
+import Hotels from "./components/Hotels";
+import EscapePenang from "./components/EscapePenang"; 
+import PenangHill from "./components/PenangHill";
+import Kekloksitemple from "./components/Kekloksitemple";
+import Pesta from "./components/Pesta";
+import Bridge from "./components/Bridge";
+import BatuFerringhi from "./components/BatuFerringhi";
+import Entopia from "./components/Entopia";
+import StreetArt from "./components/StreetArt";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <Router>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Navbar />
+        <Routes>
+          <Route 
+            path="/" 
+            element={
+              <>
+                <Hero />
+                <Popular />
+                <Foodie />
+                <Hotels />
+                <Adventure />
+                <Footer />
+              </>
+            } 
+          />
+
+          {/* Route for ESCAPE Penang */}
+          <Route path="/EscapePenang" element={<EscapePenang />} />
+          <Route path="/PenangHill" element={<PenangHill />} />
+          <Route path="/Kekloksitemple" element={<Kekloksitemple />} />
+          <Route path="/Pesta" element={<Pesta />} />
+          <Route path="/Bridge" element={<Bridge />} />
+          <Route path="/BatuFerringhi" element={<BatuFerringhi />} />
+          <Route path="/Entopia" element={<Entopia />} />
+          <Route path="/StreetArt" element={<StreetArt />} />
+          {/* Add more routes for other detailed pages as needed */}
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
